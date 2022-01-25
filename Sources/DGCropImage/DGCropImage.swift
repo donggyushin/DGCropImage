b78d9138119b6f1617fb81a55f4ef47274cde0aa
신동규 <donggyu9410@gmail.com>
2022-01-26 01:34:51 +0900

    init package

---
+++ Sources/DGCropImage/DGCropImage.swift
@@ -0,0 +1,6 @@
+public struct DGCropImage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/DGCropImageTests/DGCropImageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DGCropImage
+
+final class DGCropImageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DGCropImage().text, "Hello, World!")
+    }
+}
