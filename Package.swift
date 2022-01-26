0cf7cc65d563f871668632f39cb85e9c7c5ad913
신동규 <donggyu9410@gmail.com>
2022-01-26 15:04:06 +0900

    change style package.swift file

--- Package.swift
+++ Package.swift
@@ -6,25 +6,15 @@ import PackageDescription
 let package = Package(
     name: "DGCropImage",
     defaultLocalization: "en",
-    platforms: [
-        .iOS(.v12)
-    ],
+    platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DGCropImage",
             targets: ["DGCropImage"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DGCropImage",
-            dependencies: [],
             resources: [.process("Resources")]
         ),
         .testTarget(
--- Sources/DGCropImage/Extensions/StringExtensions.swift
+++ Sources/DGCropImage/Extensions/StringExtensions.swift
@@ -4,9 +4,7 @@
 //
 //  Created by 신동규 on 2022/01/26.
 //
-
 import Foundation
-
 extension String {
     var localized: String {
         return NSLocalizedString(self, tableName: "Localizable", bundle: .module, value: "", comment: "")
